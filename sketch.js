9ef6d41372b1e4c6a614f13f5e96a75575a76238
Trishy13 <82108022+Trishy13@users.noreply.github.com>
2021-05-09 13:01:59 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,48 @@
-var box1,box2;
+var box1, box2, box3, box4;
 
 function setup(){
-  createCanvas(600,600);
-}
 
+  createCanvas(600,600);
+
+  box1= createSprite(300,300,100,70);
+  box1.shapeColor= "crimson";
+
+  box2 = createSprite(100,100,50,100);
+  box2.shapeColor = "violet";
+
+  box3=createSprite(200,200,25,125);
+  box3.shapeColor = "blue";
+
+  box4=createSprite(400,400,75,110);
+  box4.shapeColor = "yellow";
+}
 
 function draw(){
+  background(200);
 
-background(0);
-box1=createSprite(random(5,50),10,15);
-box2=createSprite(random(5,50),15,20);
+  box2.x = World.mouseX;
+  box2.y = World.mouseY;
+
+  if(touch(box1,box2)){
+    box1.x = random(100,500);
+    box1.y = random(100,500);
+  }
+
+  if(touch(box3,box2)){
+  box3.shapeColor = "black"; 
+  }
+  
+  else{
+  box3.shapeColor = "blue";
+  }
+  if(touch(box4,box2)){
+  box4.visible=false;
+  }
+
+  else{
+    box4.visible = true;
+    
+  }
+  drawSprites();
 }
 
-drawSprites();
